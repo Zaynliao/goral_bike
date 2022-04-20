e3361bda08976c847e8ce104acf23cb027e114aa
June <barbie841111@gmail.com>
2022-04-20 18:55:19 +0800

    add course's file.

---
+++ course/api/course-doDelete.php
@@ -0,0 +1,31 @@
+<?php
+require_once("../db-connect.php");
+
+$id=$_POST["id"];
+
+
+$sql="UPDATE classes SET course_valid=0 WHERE course_id='$id'";
+
+if ($conn->query($sql) === TRUE) {
+
+    $data=[
+        "status"=>1,
+        "message"=>"下架課程成功"
+    ];
+    echo json_encode($data);
+
+} else {
+
+    $data=[
+        "status"=>0,
+        "message"=>"下架課程失敗: " . $conn->error
+    ];
+    echo json_encode($data);
+
+    exit;
+}
+
+$conn->close();
+
+
+?>
