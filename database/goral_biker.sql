ef42096aecad9258db11742d32b91d89ca3f1d11
Zaynliao <liaozayn@gmail.com>
2022-04-22 16:27:24 +0800

    資料庫更新

--- database/goral_biker.sql
+++ database/goral_biker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2022-04-21 17:17:38
+-- 產生時間： 2022-04-22 16:26:42
 -- 伺服器版本： 10.4.24-MariaDB
 -- PHP 版本： 8.1.4
 
@@ -257,6 +257,7 @@ CREATE TABLE `product` (
   `product_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `product_images` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `product_price` int(10) NOT NULL,
+  `product_update` date NOT NULL,
   `product_category_id` int(5) NOT NULL,
   `valid` int(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -265,70 +266,72 @@ CREATE TABLE `product` (
 -- 傾印資料表的資料 `product`
 --
 
-INSERT INTO `product` (`product_id`, `product_name`, `product_images`, `product_price`, `product_category_id`, `valid`) VALUES
-(1, '挑戰者300', '挑戰者300.jpg', 18800, 1, 0),
-(2, '公爵500 ', '公爵500.jpg', 118000, 1, 1),
-(3, '探索者100\r\n', '探索者100.jpg\r\n', 11800, 1, 1),
-(4, '公爵300', '公爵300.jpg', 12800, 1, 1),
-(5, '勇士500-D', '勇士500-D.jpg', 9500, 1, 1),
-(6, '勇士500-V', '勇士500-V.jpg', 8500, 1, 1),
-(7, '勇士300-DS', '勇士300-DS.jpg', 8500, 1, 1),
-(8, '勇士300-V', '勇士300-V.jpg', 7500, 1, 1),
-(9, '維多300-V', '維多300-V.jpg', 7980, 1, 1),
-(10, '維多利亞500', '維多利亞500.jpg', 7480, 1, 1),
-(11, '維多利亞600D', '維多利亞600D.jpg', 11800, 1, 1),
-(12, '維多利亞500MD', '維多利亞500MD.jpg', 9500, 1, 1),
-(13, '達卡624MD', '達卡624MD.jpg', 7280, 1, 1),
-(14, '達卡624', '達卡624.jpg', 6500, 1, 1),
-(15, 'FS90', 'FS90.jpg', 3950, 1, 1),
-(16, 'BIG_NINE7000', 'BIG_NINE7000.jpg', 118000, 2, 1),
-(17, 'BIG_NINE_XT', 'BIG_NINE_XT.jpg', 79800, 2, 1),
-(18, '探索者300', '探索者300.jpg', 14800, 1, 1),
-(19, 'BIG_NINE_5000 ', 'BIG_NINE_5000.jpg', 698000, 2, 1),
-(20, 'BIG_NINE_3000', 'BIG_NINE_3000.jpg', 52800, 2, 1),
-(21, 'BIG_NINE_700', 'BIG_NINE_700.jpg', 55000, 2, 1),
-(22, 'BIG_NINE_600', 'BIG_NINE_600.jpg', 41800, 2, 1),
-(23, 'BIG_NINE_LIMITED', 'BIG_NINE_LIMITED.jpg', 32800, 2, 1),
-(24, 'BIG_NINE_XT_EDITION', 'BIG_NINE_XT_EDITION.jpg', 49800, 2, 1),
-(25, 'BIG_NINE_XT2', 'BIG_NINE_XT2.jpg', 45000, 2, 1),
-(26, 'ONE_SIXTY_700', 'ONE_SIXTY_700.jpg', 36000, 2, 1),
-(27, 'BIG_NINE_SLX_EDITION', 'BIG_NINE_SLX_EDITION.jpg', 36000, 2, 1),
-(28, 'BIG_NINE_500', 'BIG_NINE_500.jpg', 29800, 2, 1),
-(29, 'BIG_NINE_400', 'BIG_NINE_400.jpg', 27800, 2, 1),
-(30, 'BIG_NINE_300', 'BIG_NINE_300.jpg', 23800, 2, 1),
-(31, 'BIG_NINE_200', 'BIG_NINE_200.jpg', 22000, 2, 1),
-(32, 'BIG_NINE_100_2x', 'BIG_NINE_100_2x.jpg', 19900, 2, 1),
-(33, 'BIG_NINE_100_3x', 'BIG_NINE_100_3x.jpg', 19900, 2, 1),
-(34, 'BIG_NINE_60_2x\r\n', 'BIG_NINE_60_2x.jpg', 19800, 2, 1),
-(35, 'BIG_NINE_60_3x', 'BIG_NINE_60_3x.jpg', 19800, 2, 1),
-(36, 'BIG_NINE_20_2x', 'BIG_NINE_20_2x.jpg', 16800, 2, 1),
-(37, 'BIG_NINE_20_3x', 'BIG_NINE_20_3x.jpg', 16800, 2, 1),
-(38, 'BIG_NINE_15\r\n', 'BIG_NINE_15.jpg', 14800, 2, 1),
-(39, 'BIG_SEVEN_300', 'BIG_SEVEN_300.jpg', 23800, 2, 1),
-(40, 'BIG_SEVEN_200', 'BIG_SEVEN_200.jpg', 22000, 2, 1),
-(41, 'BIG_SEVEN_100_2x', 'BIG_SEVEN_100_2x.jpg', 19900, 2, 1),
-(42, 'BIG_SEVEN_60_2x', 'BIG_SEVEN_60_2x.jpg', 19800, 2, 1),
-(43, 'BIG_SEVEN_20', 'BIG_SEVEN_20.jpg', 16800, 2, 1),
-(44, 'BIG_SEVEN_15', 'BIG_SEVEN_15.jpg', 14800, 2, 1),
-(45, 'BIG_TRAIL_600', 'BIG_TRAIL_600.jpg', 45000, 2, 1),
-(46, 'BIG_TRAIL_500', 'BIG_TRAIL_500.jpg', 35000, 2, 1),
-(47, 'BIG_TRAIL_400', 'BIG_TRAIL_400.jpg', 29900, 2, 1),
-(48, 'BIG_TRAIL_200', 'BIG_TRAIL_200.jpg', 23800, 2, 1),
-(49, 'MATTS_7_80', 'MATTS_7_80.jpg', 21800, 2, 1),
-(50, 'MATTS_7_70', 'MATTS_7_70.jpg', 20800, 2, 1),
-(51, 'MATTS_7_60_2x', 'MATTS_7_60_2x.jpg', 18000, 2, 1),
-(52, 'MATTS_7_30', 'MATTS_7_30.jpg', 16000, 2, 1),
-(53, 'MATTS_7_20', 'MATTS_7_20.jpg', 15000, 2, 1),
-(54, 'MATTS_J_CHAMPION', 'MATTS_J_CHAMPION.jpg', 22500, 2, 1),
-(55, 'MATTS_J_24', 'MATTS_J_24.jpg', 14500, 2, 1),
-(56, 'MATTS_6_10_V', 'MATTS_6_10_V.jpg', 14500, 2, 1),
-(57, 'ONE_TWENTY_600', 'ONE_TWENTY_600.jpg', 69800, 3, 1),
-(58, 'ONE_TWENTY_400', 'ONE_TWENTY_400.jpg', 51800, 3, 1),
-(59, 'ONE_FORTY_600', 'ONE_FORTY_600.jpg', 66800, 3, 1),
-(60, 'ONE_FORTY_400', 'ONE_FORTY_400.jpg', 55800, 3, 1),
-(61, 'ONE_SIXTY_700     ', 'ONE_SIXTY_700.jpg', 122445, 2, 1),
-(79, 'x500', 'BIG_NINE_500.jpg', 19880, 1, 1),
-(125, 'xsss ', '雞排.webp', 666666, 1, 1);
+INSERT INTO `product` (`product_id`, `product_name`, `product_images`, `product_price`, `product_update`, `product_category_id`, `valid`) VALUES
+(1, '挑戰者300', '挑戰者300.jpg', 18800, '2022-04-22', 1, 1),
+(2, '公爵500 ', '公爵500.jpg', 118000, '2022-04-12', 1, 1),
+(3, '探索者100\r\n', '探索者100.jpg\r\n', 11800, '2022-04-06', 1, 1),
+(4, '公爵300', '公爵300.jpg', 12800, '2022-04-20', 1, 1),
+(5, '勇士500-D', '勇士500-D.jpg', 9500, '2022-04-28', 1, 1),
+(6, '勇士500-V', '勇士500-V.jpg', 8500, '2022-04-29', 1, 1),
+(7, '勇士300-DS', '勇士300-DS.jpg', 8500, '2022-04-01', 1, 1),
+(8, '勇士300-V', '勇士300-V.jpg', 7500, '2022-05-20', 1, 1),
+(9, '維多300-V', '維多300-V.jpg', 7980, '2022-04-17', 1, 1),
+(10, '維多利亞500', '維多利亞500.jpg', 7480, '2022-04-27', 1, 1),
+(11, '維多利亞600D', '維多利亞600D.jpg', 11800, '2022-04-28', 1, 1),
+(12, '維多利亞500MD', '維多利亞500MD.jpg', 9500, '2022-04-26', 1, 1),
+(13, '達卡624MD', '達卡624MD.jpg', 7280, '2022-04-28', 1, 1),
+(14, '達卡624', '達卡624.jpg', 6500, '2022-04-20', 1, 1),
+(15, 'FS90', 'FS90.jpg', 3950, '2022-04-21', 1, 1),
+(16, 'BIG_NINE7000', 'BIG_NINE7000.jpg', 118000, '2022-04-01', 2, 1),
+(17, 'BIG_NINE_XT', 'BIG_NINE_XT.jpg', 79800, '2022-04-27', 2, 1),
+(18, '探索者300', '探索者300.jpg', 14800, '2022-04-29', 1, 1),
+(19, 'BIG_NINE_5000 ', 'BIG_NINE_5000.jpg', 698000, '2022-04-27', 2, 1),
+(20, 'BIG_NINE_3000', 'BIG_NINE_3000.jpg', 52800, '2022-04-30', 2, 1),
+(21, 'BIG_NINE_700', 'BIG_NINE_700.jpg', 55000, '2022-05-12', 2, 1),
+(22, 'BIG_NINE_600', 'BIG_NINE_600.jpg', 41800, '2022-05-17', 2, 1),
+(23, 'BIG_NINE_LIMITED', 'BIG_NINE_LIMITED.jpg', 32800, '2022-04-27', 2, 1),
+(24, 'BIG_NINE_XT_EDITION', 'BIG_NINE_XT_EDITION.jpg', 49800, '2022-04-22', 2, 1),
+(25, 'BIG_NINE_XT2', 'BIG_NINE_XT2.jpg', 45000, '2022-04-27', 2, 1),
+(26, 'ONE_SIXTY_700', 'ONE_SIXTY_700.jpg', 36000, '2022-04-22', 2, 1),
+(27, 'BIG_NINE_SLX_EDITION', 'BIG_NINE_SLX_EDITION.jpg', 36000, '2022-04-02', 2, 1),
+(28, 'BIG_NINE_500', 'BIG_NINE_500.jpg', 29800, '2022-04-20', 2, 1),
+(29, 'BIG_NINE_400', 'BIG_NINE_400.jpg', 27800, '2022-04-13', 2, 1),
+(30, 'BIG_NINE_300', 'BIG_NINE_300.jpg', 23800, '2022-04-13', 2, 1),
+(31, 'BIG_NINE_200', 'BIG_NINE_200.jpg', 22000, '2022-04-27', 2, 1),
+(32, 'BIG_NINE_100_2x', 'BIG_NINE_100_2x.jpg', 19900, '2022-04-28', 2, 1),
+(33, 'BIG_NINE_100_3x', 'BIG_NINE_100_3x.jpg', 19900, '2022-04-28', 2, 1),
+(34, 'BIG_NINE_60_2x\r\n', 'BIG_NINE_60_2x.jpg', 19800, '2022-05-18', 2, 1),
+(35, 'BIG_NINE_60_3x', 'BIG_NINE_60_3x.jpg', 19800, '2022-04-27', 2, 1),
+(36, 'BIG_NINE_20_2x', 'BIG_NINE_20_2x.jpg', 16800, '2022-04-28', 2, 1),
+(37, 'BIG_NINE_20_3x', 'BIG_NINE_20_3x.jpg', 16800, '2022-04-29', 2, 1),
+(38, 'BIG_NINE_15\r\n', 'BIG_NINE_15.jpg', 14800, '2022-06-01', 2, 1),
+(39, 'BIG_SEVEN_300', 'BIG_SEVEN_300.jpg', 23800, '2022-06-02', 2, 1),
+(40, 'BIG_SEVEN_200', 'BIG_SEVEN_200.jpg', 22000, '2022-04-28', 2, 1),
+(41, 'BIG_SEVEN_100_2x', 'BIG_SEVEN_100_2x.jpg', 19900, '2022-05-16', 2, 1),
+(42, 'BIG_SEVEN_60_2x', 'BIG_SEVEN_60_2x.jpg', 19800, '2022-04-26', 2, 1),
+(43, 'BIG_SEVEN_20', 'BIG_SEVEN_20.jpg', 16800, '2022-04-28', 2, 1),
+(44, 'BIG_SEVEN_15', 'BIG_SEVEN_15.jpg', 14800, '2022-04-26', 2, 1),
+(45, 'BIG_TRAIL_600', 'BIG_TRAIL_600.jpg', 45000, '2022-04-27', 2, 1),
+(46, 'BIG_TRAIL_500', 'BIG_TRAIL_500.jpg', 35000, '2022-04-25', 2, 1),
+(47, 'BIG_TRAIL_400', 'BIG_TRAIL_400.jpg', 29900, '2022-04-24', 2, 1),
+(48, 'BIG_TRAIL_200', 'BIG_TRAIL_200.jpg', 23800, '2022-04-24', 2, 1),
+(49, 'MATTS_7_80', 'MATTS_7_80.jpg', 21800, '2022-04-24', 2, 1),
+(50, 'MATTS_7_70', 'MATTS_7_70.jpg', 20800, '2022-04-25', 2, 1),
+(51, 'MATTS_7_60_2x', 'MATTS_7_60_2x.jpg', 18000, '2022-04-24', 2, 1),
+(52, 'MATTS_7_30', 'MATTS_7_30.jpg', 16000, '2022-04-16', 2, 1),
+(53, 'MATTS_7_20', 'MATTS_7_20.jpg', 15000, '2022-04-26', 2, 1),
+(54, 'MATTS_J_CHAMPION', 'MATTS_J_CHAMPION.jpg', 22500, '2022-04-24', 2, 1),
+(55, 'MATTS_J_24', 'MATTS_J_24.jpg', 14500, '2022-04-30', 2, 1),
+(56, 'MATTS_6_10_V', 'MATTS_6_10_V.jpg', 14500, '2022-04-15', 2, 1),
+(57, 'ONE_TWENTY_600', 'ONE_TWENTY_600.jpg', 69800, '2022-04-15', 3, 1),
+(58, 'ONE_TWENTY_400', 'ONE_TWENTY_400.jpg', 51800, '2022-04-01', 3, 1),
+(59, 'ONE_FORTY_600', 'ONE_FORTY_600.jpg', 66800, '2022-04-22', 3, 1),
+(60, 'ONE_FORTY_400', 'ONE_FORTY_400.jpg', 55800, '2022-04-15', 3, 1),
+(61, 'ONE_SIXTY_700     ', 'ONE_SIXTY_700.jpg', 122445, '2022-03-24', 2, 1),
+(79, 'x500', 'BIG_NINE_500.jpg', 19880, '2022-04-13', 1, 1),
+(125, 'xsss ', '雞排.webp', 666666, '2022-04-22', 1, 1),
+(126, 'xsss ', '雞排.webp', 666666, '2022-04-22', 1, 1),
+(127, 'TEST02 ', '滷雞腿.webp', 4564545, '2022-04-22', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -405,7 +408,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `account`, `password`, `gender`, `birthday`, `address`, `email`, `phone_number`, `create_time`, `enable`) VALUES
-(1, 'Eason', '12345', '$2y$10$mgy3i0/HNDwKEGx48/REFeDrxouzH/.FhVzKCY9ZAJ6G3sF2ydc6u', 'male', '2022-04-18', '臺南市永康區永忠路28號', 'yeji@test.com', '0945654789', '2022-04-19 19:29:06', 1),
+(1, 'Eason', '12345', '$2y$10$mgy3i0/HNDwKEGx48/REFeDrxouzH/.FhVzKCY9ZAJ6G3sF2ydc6u', 'male', '2022-04-18', '臺南市永康區永忠路28號', 'yeji@test.com', '0945654789', '2022-04-19 19:29:06', 0),
 (2, 'sara', '54321', '$2y$10$/KxuCIh/Wku1hKyDnLKC6OgzzdICW6wQIA75SyAkFB3aBw8K5WpQu', 'male', '2022-04-11', '臺南市鹽水區竹圍33號', 'sara@test.com', '0956123456', '2022-04-19 19:35:42', 1),
 (3, 'wade', 'aa1234', '$2y$10$2OWH8T1CA24WZAZOMR/BMuFdFQfSVvMdrO3vFRfMb2L8D18FPQ6xC', 'male', '2022-04-19', '桃園市觀音區富國街33號', 'tom@test.com', '0956123478', '2022-04-20 15:05:58', 1),
 (4, 'mike', '99878', '$2y$10$vs0jkE.W/uukDUPpkCAGu.y4nuT72wo3I4yGBvHZzW4wEBvmZJBW2', 'male', '2022-04-13', '臺中市南屯區大墩西二街23號', 'mike@test.com', '0956123478', '2022-04-20 15:06:45', 1),
@@ -541,7 +544,7 @@ ALTER TABLE `payment_method`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=126;
+  MODIFY `product_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `product_category`
