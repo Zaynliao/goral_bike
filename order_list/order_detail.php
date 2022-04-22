b78c0c71b2a450ce60957cbb01e7acd7ffd807c4
June <barbie841111@gmail.com>
2022-04-22 21:02:45 +0800

    訂單樣式

--- order_list/order_detail.php
+++ order_list/order_detail.php
@@ -18,8 +18,10 @@ $sql = "SELECT
 payment_method.payment_method_name,
 `coupons`.`coupon_name`,
 `coupons`.`coupon_content`,
-`order_list`.`total_amount`
-FROM order_list,user,payment_method,coupons 
+`order_list`.`total_amount`,
+`product_category`.`product_category_id`,
+`product_category`.`product_category_name`
+FROM order_list,user,payment_method,coupons,`product_category`
 WHERE `order_list`.`order_id`='$order_id'
 AND `order_list`.`user_id`=`user`.`id` 
 AND `order_list`.`payment_method_id`=`payment_method`.`id` 
@@ -30,7 +32,13 @@ AND `order_list`.`coupon_id`=`coupons`.`id` ";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 
-$sql_product = "SELECT `product`.`product_name`,`product`.`product_images`,`product`.`product_price` FROM product_order,product,order_list WHERE  `order_list`.`order_id`=`product_order`.`order_id` AND `product_order`.`product_id`=`product`.`product_id` AND `order_list`.`order_id`='$order_id'";
+$sql_product = "SELECT `product`.`product_name`,`product`.`product_images`,`product`.`product_price` ,`product_category_name`
+FROM product_order,product,order_list ,`product_category`
+WHERE  `order_list`.`order_id`=`product_order`.`order_id` 
+AND `product_order`.`product_id`=`product`.`product_id` 
+AND `order_list`.`order_id`='$order_id'
+AND `product_category`.`product_category_id`=`product`.`product_category_id` 
+";
 
 $result_product = $conn->query($sql_product);
 $rows = $result_product->fetch_all(MYSQLI_ASSOC);
@@ -43,56 +51,37 @@ $conn->close();
 
 // echo json_encode($row);
 
+if($row["order_status"]==1){
+    $statusName="已付款";
+}else{
+    $statusName="未付款";
+}
+
 ?>
 
 
 <div class="container">
-
-    <div class="mt-5 card">
-
-        <p>訂單編號 : <?= $row["order_id"] ?> </p>
-        <p>使用者姓名 : <?= $row["name"] ?></p>
-        <p>寄送地址 : <?= $row["address"] ?></p>
-        <p>訂單狀態 : <?= $row["order_status"] ?></p>
-        <p>訂單創建時間 : <?= $row["order_create_time"] ?></p>
-
-        <ul>
-            <div class="row">
+    <p class="badge mb-0 mt-4 text-dark d-flex justify-content-end">訂單創建時間 : <?= $row["order_create_time"] ?></p>
+    <div class="card shadow-sm p-4 fw-bold lh-1 bg-light">
+        <p class="badge bg-secondary fs-6">訂單編號 : <?= $row["order_id"] ?></p>
+        <div class="p-4">
+            <p>使用者姓名 : <?= $row["name"] ?></p>
+            <p>寄送地址 : <?= $row["address"] ?></p>
+            <p>訂單狀態 : <?= $statusName ?></p>
+            <ul class="card shadow-sm my-5 py-3 ps-0">
                 <?php foreach ($rows as $row_product) : ?>
-
-
-                    <div class="col-lg-3 col-md-6 col-sm-6 mb-4">
-
-                        <div class="card p-2">
-
-
-                            <figure class=" figure d-flex justify-content-center align-items-center" style="height: 240px;">
-                                <img class="img-fluid" src="../product/goral_bike_pic/<?= $row_product["product_images"] ?>" alt="">
-                            </figure>
-
-
-
-                            <h1 class="text-center h4"><?= $row_product["product_name"] ?></h1>
-
-                            <div class="py-2 px-3">
-                                <div class="price text-end">
-                                    $<?= $row_product["product_price"] ?>
-                                </div>
-                            </div>
-
-
-                        </div>
-                    </div>
-
-
-
-
+                <div class="d-flex align-items-center gap-4 justify-content-evenly">
+                    <img class="w-25" src="../product/goral_bike_pic/<?= $row_product["product_images"] ?>" alt="">
+                    <p><?= $row_product["product_name"] ?></p>
+                    <p><?= $row_product["product_category_name"] ?></p>
+                    <p>$<?= $row_product["product_price"] ?></p>
+                </div>
+                <hr>
                 <?php endforeach; ?>
-            </div>
-        </ul>
-        <p>付款方式 : <?= $row["payment_method_name"] ?></p>
-        <p>優惠碼 : <?= $row["coupon_name"] ?></p>
-        <p>優惠碼內容 : <?= $row["coupon_content"] ?></p>
-        <p>總金額 : <?= $row["total_amount"] ?></p>
-
+            </ul>
+            <p>付款方式 : <?= $row["payment_method_name"] ?></p>
+            <p>優惠碼 : <?= $row["coupon_name"] ?></p>
+            <p>優惠碼內容 : <?= $row["coupon_content"] ?></p>
+            <p>總金額 : <?= $row["total_amount"] ?></p>
+        </div>
     </div>
