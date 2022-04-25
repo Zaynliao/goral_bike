8b13acfe73b8f8acf7882a2f57dc99ab07b54380
Zaynliao <liaozayn@gmail.com>
2022-04-25 10:24:15 +0800

    修改優惠卷 付款方式 aside

--- goral_bike_layout/goral_biker_coupons.php
+++ goral_bike_layout/goral_biker_coupons.php
@@ -20,7 +20,7 @@
         </header>
         <section class="row p-0 m-0 vh-100">
             <aside class="aside col-2 p-0">
-                <?php require("../aside/product_aside.php") ?>
+                <?php require("../aside/coupons_aside.php") ?>
             </aside>
             <article class="article col-10 ">
                 <?php
